b48214ddccd0ac5751b9c6b3274147526b053051
Thayana Classo Soares <t.classo.soares@avanade.com>
2023-05-31 19:05:20 -0300

    wip redo navbar responsive

--- src/components/patterns/Navbar/index.js
+++ src/components/patterns/Navbar/index.js
@@ -47,93 +47,124 @@ export default function Navbar() {
   }
 
   return (
-    <div className="flex box-border">
-      <div class="mob-header fixed right-5 top-4 z-50 hidden lg:shadow-slate-900 lg-z-50 lg:w-full lg:flex">
-        <button
-          onClick={handleClick}
-          className="toggler-menu cursor-none w-11 h-11 relative rounded-full p-0
-       bg-black border-none ml-auto"
-        >
-          <div className="menu-hamb">
-            <span className="first:-top-4 absolute left-0 bottom-0 right-0 w-6 h-0.5 m-auto bg-white text-white transition duration-150"></span>
-            <span className="even:-top-px absolute left-0 bottom-0 right-0 w-6 h-0.5 m-auto bg-white text-white transition duration-150"></span>
-            <span className="last:top-auto absolute left-0 bottom-3 right-0 w-6 h-0.5 m-auto bg-white text-white transition duration-150"></span>
-          </div>
-        </button>
-      </div>
-
-      {!toggle && (
-        <div
-          className="inner-navbar bg-transparent flex items-center justify-between w-full px-10 h-20
-          lg:px-5 lg:w-80 lg:h-screen lg:fixed lg:top-0 lg:left-0 lg:bg-slate-100 lg:clear-both lg:pt-8
-          lg:pr-5 lg:pb-5 lg:shadow-indigo-500/40 lg:z-50
-          lg:flex lg:flex-col lg:translate-x-6 lg:transition-all lg:ease-in-out"
-        >
-          <div className="avatar-img flex items-center">
-            <img
-              className="rounded-full max-h-20 lg:max-h-28 lg:border-2 lg:border-slate-400"
-              src="https://avatars.githubusercontent.com/u/31221444?v=4"
-            ></img>
-          </div>
-
-          <div
-            className="navbar text-slate-600 h-auto font-medium font-sans
-       lg:w-full lg:flex lg:justify-start lg:h-3/4 lg:border-b-2 lg:border-slate-400 lg:z-40"
-          >
-            <ul
-              className="anchor-nav flex items-center
-          lg:items-baseline lg:flex-col lg:text-gray-800 lg: align-baseline lg:fixed lg:bottom-0
-          lg:top-44 lg:w-1/3"
+    <div className="home-light">
+      <header
+        className="header-area none fixed w-full left-0 top-0 bg-white z-50 shadow-slate-950
+      lg:block"
+      >
+        <div className="header-inner w-2/4 max-w-6xl py-0 px-3">
+          <div className="logo">
+            <a
+              href="/"
+              className="transition-all duration-3000 ease-in-out cursor-pointer"
             >
-              <li className="list_inner py-2 px-2">
-                <a
-                  className="flex items-center text-base py-3 px-2.5 leading-none border rounded border-none hover:border-transparent hover:bg-slate-100 inset-x-0 inset-y-0"
-                  href="/"
-                >
-                  <House />
-                  Home
-                </a>
-              </li>
-              <li class="list_inner py-2 px-2">
-                <a
-                  className="flex items-center text-base py-3 px-2.5 leading-none border rounded border-none hover:border-transparent hover:bg-slate-100 inset-x-0 inset-y-0"
-                  href="/about"
-                >
-                  <Avatar />
-                  About
-                </a>
-              </li>
-              <li class="list_inner py-2 px-2">
-                <a
-                  className="flex items-center text-base py-3 px-2.5 leading-none border rounded border-none hover:border-transparent hover:bg-slate-100 inset-x-0 inset-y-0"
-                  href="/projects"
-                >
-                  <Portfolio />
-                  Projects
-                </a>
-              </li>
-              <li class="list_inner py-2 px-2">
-                <a
-                  className="flex items-center text-base py-3 px-2.5 leading-none border rounded border-none hover:border-transparent hover:bg-slate-100 inset-x-0 inset-y-0"
-                  href="/music"
-                >
-                  <Music />
-                  Music
-                </a>
-              </li>
-              <li class="list_inner py-2 px-2">
-                <a
-                  className="flex items-center text-base py-3 px-2.5 leading-none border rounded border-none hover:border-transparent hover:bg-slate-100 inset-x-0 inset-y-0"
-                  href="/music"
-                >
-                  <Contact />
-                  Contact
-                </a>
-              </li>
-            </ul>
+              {" "}
+              <img
+                className="rounded-full max-h-20 lg:max-h-28 lg:border-2 lg:border-slate-800 max-w-xs"
+                src="https://avatars.githubusercontent.com/u/31221444?v=4"
+              ></img>
+            </a>
           </div>
         </div>
-      )}
+      </header>
+      <div className="horizontal-menu">
+        <div
+          className="mob-header none fixed right-5 top-4 z-50 hidden lg:shadow-slate-900
+        lg-z-50 lg:block lg:fixed lg:right-5 lg:top-3 lg:z-50"
+        >
+          <button
+            onClick={handleClick}
+            className="toggler-menu cursor-none w-11 h-11 relative rounded-full p-0
+       bg-black border-none ml-auto"
+          >
+            <div className="menu-hamb">
+              <span className="first:-top-4 absolute left-0 bottom-0 right-0 w-6 h-0.5 m-auto bg-white text-white transition duration-150"></span>
+              <span className="even:-top-px absolute left-0 bottom-0 right-0 w-6 h-0.5 m-auto bg-white text-white transition duration-150"></span>
+              <span className="last:top-auto absolute left-0 bottom-3 right-0 w-6 h-0.5 m-auto bg-white text-white transition duration-150"></span>
+            </div>
+          </button>
+        </div>
+        <div
+          className="sidebar h-auto flex justify-end w-full static bg-transparent
+          lg:w-80 lg:fixed lg:top-0 lg:left-0 lg:bg-slate-600 lg:z-100 lg:transition-all
+          lg:duration-3000 lg:ease-in-out lg:h-screen"
+        >
+          <div
+            class="sidebar-inner h-20 flex items-center justify-between py-0 lg:flex-col
+          "
+          >
+            <div className="menu z-50 h-auto p-0 w-full">
+              <div
+                className="list text-slate-600 h-auto font-medium font-sans"
+                data-nav="list"
+              >
+                <ul className="anchor-nav flex items-center justify-center m-0">
+                  <li className="w-auto float-none ml-1">
+                    <div className="list-inner">
+                      <a
+                        className="text-sm font-semibold justify-center py-1 px-5 flex items-end text-zinc-900
+                       rounded-md relative transition-all duration-3000 ease-in-out lg:py-1 lg:px-3 lg:text-sm"
+                        href="/"
+                      >
+                        <Avatar />
+                        Avatar
+                      </a>
+                    </div>
+                  </li>
+                  <li className="w-auto float-none ml-1">
+                    <div className="list-inner">
+                      <a
+                        className="text-sm font-semibold justify-center py-1 px-5 flex items-end text-zinc-900
+                       rounded-md relative transition-all duration-3000 ease-in-out lg:py-1 lg:px-3 lg:text-sm"
+                        href="/"
+                      >
+                        <House />
+                        Home
+                      </a>
+                    </div>
+                  </li>
+                  <li className="w-auto float-none ml-1">
+                    <div className="list-inner">
+                      <a
+                        className="text-sm font-semibold justify-center py-1 px-5 flex items-end text-zinc-900
+                       rounded-md relative transition-all duration-3000 ease-in-out lg:py-1 lg:px-3 lg:text-sm"
+                        href="/"
+                      >
+                        <Portfolio />
+                        Portfolio
+                      </a>
+                    </div>
+                  </li>
+                  <li className="w-auto float-none ml-1">
+                    <div className="list-inner">
+                      <a
+                        className="text-sm font-semibold justify-center py-1 px-5 flex items-end text-zinc-900
+                       rounded-md relative transition-all duration-3000 ease-in-out lg:py-1 lg:px-3 lg:text-sm"
+                        href="/"
+                      >
+                        <Music />
+                        Music
+                      </a>
+                    </div>
+                  </li>
+                  <li className="w-auto float-none ml-1">
+                    <div className="list-inner">
+                      <a
+                        className="text-sm font-semibold justify-center py-1 px-5 flex items-end text-zinc-900
+                       rounded-md relative transition-all duration-3000 ease-in-out lg:py-1 lg:px-3 lg:text-sm"
+                        href="/"
+                      >
+                        <Contact />
+                        Contact
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  content: [
-    "./src/**/*.js",
-  ],
+  content: ["./src/**/*.js"],
   theme: {
     fontFamily: {
-      'body': ['"Poppins"'],
+      body: ['"Poppins"'],
     },
     screens: {
-      'lg': { 'max': '1199px'},
+      lg: { max: "1199px" },
       // => @media (max-width: 1280px) { ... }
 
-      'md': {'max': '940px'},
+      md: { max: "940px" },
       // => @media (min-width: 640px) { ... }
 
-      'sm': {'max': '576px'}
-
+      sm: { max: "576px" },
     },
 
-    safelist: ['animate-[fade-in_1s_ease-in-out]', 'animate-[fade-in-down_1s_ease-in-out]']
+    safelist: [
+      "animate-[fade-in_1s_ease-in-out]",
+      "animate-[fade-in-down_1s_ease-in-out]",
+    ],
+    zIndex: {
+      0: 0,
+      10: 10,
+      20: 20,
+      30: 30,
+      40: 40,
+      50: 50,
+      25: 25,
+      75: 75,
+      100: 100,
+      auto: "auto",
+    },
   },
-}
+};
